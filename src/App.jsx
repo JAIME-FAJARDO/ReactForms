d221d2e6d9fb5a7782c24b654ee5a0495a5b0e2b
Jaime C. Fajardo <juncfaj@gmail.com>
2023-07-25 19:45:41 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Authenticate from "./authenticate";
+import SignUpForm from "./signupform";
 
-import './App.css'
+import './App.css';
+
+const [token, setToken] = useState(null);
 
 function App() {
-  
+
   return (
     <>
-      <div>
-        <h1>React Forms</h1>        
-      </div>
- 
-          
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
-  )
+  );
+  
 }
+
 
 export default App
